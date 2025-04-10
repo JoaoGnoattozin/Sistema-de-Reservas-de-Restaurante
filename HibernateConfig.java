4f616882e7a939e83ad6fb2d9cda253108fc1320
João Pedro Quadro Carvalho <jpgnoatto15@gmail.com>
2025-04-10 19:51:40 -0300

    Create HibernateConfig.java

---
+++ HibernateConfig.java
@@ -0,0 +1,16 @@
+package com.restaurante.config;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateConfig {
+    
+    private static EntityManagerFactory emf;
+    
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (emf == null) {
+            emf = Persistence.createEntityManagerFactory("restaurante-pu");
+        }
+        return emf;
+    }
+}
